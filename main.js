742528da3587a52f54c4b2a0d9452ac0f39b6362
Mohamed Aboalmkarem <mo3ddeedd67983@gmail.com>
2024-01-05 23:41:39 +0200

    about to finish

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ let dragBox = document.querySelector('.drag-box');
 let btn = document.getElementById('btn');
 let dropBox = document.querySelector('.drop-box');
 let shapes = document.querySelectorAll('.shape');
+let range = document.getElementById('rng');
+let label = document.getElementById('label');
+// range.value = 5;
+let changeShape = null;
+let shapeStyle = null;
 
 btn.addEventListener('click', () => {
     dragBox.style.width = "100px";
@@ -21,12 +26,40 @@ for(let i = 0; i < shapes.length; i++) {
         
         shapes[i].style.left = `${left + movementX}px`;
         shapes[i].style.top = `${top + movementY}px`;
-        shapes[i].style.background = 'blue';
-        console.log(left, top)
+        // shapes[i].style.background = 'blue';
+        
+        // shapes[i].style.width, shapes[i].style.height = range.value;
+        // label.innerText = `Size: ${range.value}`;
     }
 
+    
+    // range.onchange = changeSize;
+
+    function changeSize(shape) {
+        label.innerText = `Size: ${range.value}`
+        shape.style.width = `${range.value}px`
+        shape.style.height = `${range.value}px`
+        shape.style.background = 'red';
+    }
+
+    function activeShape(shape) {
+        if (shape.classList.contains("active") == true) {
+            // console.log(true)
+            changeShape = shape;
+            // changeSize(changeShape)
+        } else {
+            return;
+        }
+    }
+    
+    
     shapes[i].addEventListener('mousedown', () => {
         shapes[i].classList.add("active");
+        shapeStyle = window.getComputedStyle(shapes[i])
+        shapes[i].style.background = '#fff'
+        range.value = parseInt(shapeStyle.width);
+        activeShape(shapes[i])
+        label.innerText = `Size: ${range.value}`
         shapes[i].addEventListener('mousemove', onDrag);
     })
 
@@ -35,3 +68,24 @@ for(let i = 0; i < shapes.length; i++) {
         shapes[i].removeEventListener('mousemove', onDrag);
     })
 }
+
+range.addEventListener('mouseenter', () => {
+    
+    console.log()
+    range.addEventListener('mousemove', () => {
+        changeSize(changeShape);
+    }) 
+})
+
+// console.log(shapes.classList. ("active"))
+// if (shapes[i].classList.contains("active") == true) {
+//     changeSize();
+// } else {
+//     return;
+// }
+
+
+// sitInterval(() => {
+//     range.value++;
+//     label.innerText = `Size: ${range.value}`
+// }, 100)
